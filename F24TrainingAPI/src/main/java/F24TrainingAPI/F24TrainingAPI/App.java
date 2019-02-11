c7e88389d1de3542d78777dc1b52f504d8aa4c33
GBS05043 <GBS05043@SVILP077.SG.GBS.PRO>
2019-02-11 13:20:45 +0530

    firstcommit

---
+++ F24TrainingAPI/src/main/java/F24TrainingAPI/F24TrainingAPI/App.java
@@ -0,0 +1,13 @@
+package F24TrainingAPI.F24TrainingAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
